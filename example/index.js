149e30be7b809cb6b99dc5f04e1bf03cc043c4f2
Puneet Singh <puneet.fm@gmail.com>
2021-04-08 19:32:40 +0530

    add update flow

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const methodOverride = require('method-override');
 const debug = require('debug')('s3DB');
 const { db } = require('../index');
 const app = express()
-const port = 3000
+const port = 3030
 
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -39,8 +39,8 @@ app.post('/api/insert', async (req, res) => {
     debug({ payload });
     const params = {
       database: 'test-s3-db',
-      collection: 'settings',
-      documentID: 'doc2',
+      collection: 'order_manager_settings',
+      documentID: '1672',
       newDoc: payload,
       metaData: {
         ContentType: 'application/json'
@@ -49,7 +49,32 @@ app.post('/api/insert', async (req, res) => {
     const data = await db.insert(params);
     return res.send({ status: 'ok', message: data });
   } catch (e) {
-    return res.status(e.statusCode).send({
+    return res.status(e.statusCode || 500).send({
+      error: e.message,
+      status: e.statusCode,
+      message: ""
+    });
+  }
+});
+
+
+app.post('/api/update', async (req, res) => {
+  try {
+    const payload = req.body;
+    debug({ payload });
+    const params = {
+      database: 'test-s3-db',
+      collection: 'order_manager_settings',
+      documentID: '1672',
+      updateDoc: payload,
+      metaData: {
+        ContentType: 'application/json'
+      }
+    }
+    const data = await db.update(params);
+    return res.send({ status: 'ok', message: data });
+  } catch (e) {
+    return res.status(e.statusCode || 500).send({
       error: e.message,
       status: e.statusCode,
       message: ""
--- src/db.js
+++ src/db.js
@@ -24,6 +24,17 @@ const getDocument = async ({ database, collection, documentID }) => {
 
 const insert = async ({ database, collection, documentID, newDoc, metaData }) => {
   const params = { database, collection, documentID, newDoc, metaData };
+  try {
+    const checkIfDocExists = await DB.getFromS3(params);
+    const error = {
+      message: `document with id ${documentID} already exists`,
+      statusCode: 400,
+      code: 'DUPLICATE_REQUEST'
+    }
+    if (checkIfDocExists) return Promise.reject(error);
+  } catch (err) {
+    debug("document doesn't exist, creating new document");
+  }
   return await DB.addToS3(params);
 }
 
@@ -32,8 +43,14 @@ const remove = async ({ database, collection, documentID }) => {
   return await DB.deleteFromS3(params)
 }
 
-const update = async ({ database, collection, documentID, document, metaData }) => {
-
+const update = async ({ database, collection, documentID, updateDoc, metaData }) => {
+  const params = { database, collection, documentID, newDoc: updateDoc, metaData };
+  try {
+    await DB.getFromS3(params)
+    return await DB.addToS3(params);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 
